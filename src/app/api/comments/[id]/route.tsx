d70f2d99821887641812cd7619bdb5492ec00192
Trinh Tuan <trinhtuan2801@gmail.com>
2024-06-30 23:42:21 +0700

    patch comment

--- src/app/api/comments/[id]/route.tsx
+++ src/app/api/comments/[id]/route.tsx
@@ -2,6 +2,18 @@ import comments from '@/mocks/comments';
 import { NextRequest } from 'next/server';
 
 export async function GET(_: NextRequest, { params }: { params: { id: string } }) {
-  const comment = comments.find((c) => c.id === Number(params.id));
-  return Response.json(comment);
+  try {
+    const comment = comments.find((c) => c.id === Number(params.id));
+    return Response.json(comment);
+  } catch (err) {
+    return new Response(undefined, { status: 404 });
+  }
+}
+
+export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
+  const body = await req.json();
+  const { text } = body;
+  const index = comments.findIndex((c) => c.id === Number(params.id));
+  comments[index].text = text;
+  return Response.json(comments[index]);
 }
